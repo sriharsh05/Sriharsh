a8ad7a219705671c6c9af800e344ce9a2b5e83f4
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-12-27 21:50:26 +0530

    update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = ({ toggle }) => {
         <NavBtn>
           <a
             className="btn PrimaryBtn"
-            href="https://linkedin.com/in/pedro-sales-muniz"
+            href="https://bit.ly/resume_sriharsh"
             target="_blank"
             rel="noopener noreferrer"
           >
