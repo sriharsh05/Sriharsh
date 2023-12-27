d4b4240fca51abb12760d1edcdd769c944bea0c0
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-12-27 21:35:32 +0530

    update introduction

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -32,7 +32,7 @@ function Hero() {
               <TypeAnimation
                 cursor={false}
                 sequence={[
-                  'Hi, I\'m Pedro.',
+                  'Hi, I\'m Sriharsh.',
                   () => setShowSubtitle(true)
                 ]}
                 speed={{ type: "keyStrokeDelayInMs", value: 150 }}
@@ -44,40 +44,24 @@ function Hero() {
                   cursor={true}
                   sequence={[
                     500,
-                    'A Full-Stack Developer.',
+                    'A Full Stack Developer.',
                     1000,
-                    'A BCIT graduate.',
+                    'A VCE student',
                     // 'I design and code beautifully simple things, and I love what I do.',
                     1000,
-                    'A problem solver.',
+                    'A tech enthusiate.',
                     1000,
                     'An innovative thinker.',
                     1000,
                     'A....',
                     1000,
-                    'A.... cool guy?',
+                    'A.... cool guy ig?',
                     1000,
                     "Ok...",
                     1000,
                     "Ok...  I'm running out of ideas...",
                     1000,
-                    "Uhh...",
-                    1000,
-                    "Uhh... you can scroll down to see my projects now...",
-                    300,
-                    () => setShowScrollDown(true),
-                    1000,
-                    "Seriously, my projects are really cool, go check them out!",
-                    1000,
-                    "You're uh...",
-                    1000,
-                    "You're uh... still here?",
-                    1000,
                     "Ok, this has been fun, but I'm gonna restart the loop now...",
-                    // 1000,
-                    // "Or...",
-                    // 1000,
-                    // "Or... I could scroll you by force! Muahaha!",
                     1000,
                     "See ya! :)",
                     500,
