8025d96a029e91899d7dd7c8b27866d909369283
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-12-29 23:04:55 +0530

    update resume link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = ({ toggle }) => {
         <NavBtn>
           <a
             className="btn PrimaryBtn"
-            href="https://bit.ly/resume_sriharsh"
+            href="https://drive.google.com/drive/folders/1JSuRzX7zwUprBd8C2q2HKk0vbXdSgB1D"
             target="_blank"
             rel="noopener noreferrer"
           >
