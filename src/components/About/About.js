4712b4d7766bb96025b5b9b668fe406b9b83c239
sriharsh05 <sriharsh.harsha5@gmail.com>
2024-09-25 22:58:53 +0530

    Change about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,125 +22,148 @@ function About() {
         <div className="SectionTitle">About Me</div>
         <div className="BigCard">
           {/* <ScrollAnimation animateIn="fadeInLeft"> */}
-            <Image src="/man-svgrepo-com.svg" alt="man-svgrepo" />
+          <Image src="/man-svgrepo-com.svg" alt="man-svgrepo" />
           {/* </ScrollAnimation> */}
           <div className="AboutBio">
-              <VerticalTimeline>
+            <VerticalTimeline>
               <VerticalTimelineElement
-                  className="vertical-timeline-element--work"
-                  contentStyle={{
-                    background: "#1f163e",
-                    color: "#fff",
-                  }}
-                  contentArrowStyle={{
-                    borderRight: "7px solid  rgb(33, 150, 243)",
-                  }}
-                  date="Feb 2024 - present"
-                  iconStyle={{ background: "#1f163e", color: "#fff" }}
-                  icon={WorkIcon}
-                >
-                  <h3 className="vertical-timeline-element-title">
-                    ATSE intern
-                  </h3>
-                  <h4 className="vertical-timeline-element-subtitle">
-                    Salesforce India
-                  </h4>
-                  <p>
-                  Global leader in CRM software, driving customer relationships and 
-                  business growth through cloud-based solutions and social responsibility initiatives.
-                  </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                  className="vertical-timeline-element--work"
-                  contentStyle={{
-                    color: "#000",
-                  }}
-                  contentArrowStyle={{
-                    borderRight: "7px solid  rgb(33, 150, 243)",
-                  }}
-                  date="Aug 2023 - Feb 2024"
-                  iconStyle={{ background: "#1f163e", color: "#fff" }}
-                  icon={WorkIcon}
-                >
-                  <h3 className="vertical-timeline-element-title">
-                    National Software Engineering Fellow (GDC intern)
-                  </h3>
-                  <h4 className="vertical-timeline-element-subtitle">
-                    EGOVERNMENT FOUNDATION & PUPILFIRST PRIVATE LIMITED
-                  </h4>
-                  <p>
-                    Deputed to eGovernment foundation to develop Tele-ICU
-                    tooling for 10bedICU project.
-                  </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                  className="vertical-timeline-element--work"
-                  contentStyle={{
-                    color: "#000",
-                    marginRight: "10px",
-                  }}
-                  date="2020 - 2024"
-                  iconStyle={{ background: "#1f163e", color: "#fff" }}
-                  icon={SchoolIcon}
-                >
-                  <h3 className="vertical-timeline-element-title">
-                    Bachelor of Technology
-                  </h3>
-                  <h4 className="vertical-timeline-element-subtitle">
-                    Information Technology
-                  </h4>
-                  <p>Vardhaman college of Engineering, Hyderabad</p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                  className="vertical-timeline-element--work"
-                  contentStyle={{
-                    color: "#000",
-                  }}
-                  date="2018 - 2020"
-                  iconStyle={{ background: "#1f163e", color: "#fff" }}
-                  icon={SchoolIcon}
-                >
-                  <h3 className="vertical-timeline-element-title">
-                    Board of Intermediate
-                  </h3>
-                  <p>Narayana IIT academy</p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                  className="vertical-timeline-element--work"
-                  contentStyle={{
-                    color: "#000",
-                    marginRight: "10px",
-                  }}
-                  date="2018 "
-                  iconStyle={{ background: "#1f163e", color: "#fff" }}
-                  icon={SchoolIcon}
-                >
-                  <h3 className="vertical-timeline-element-title">
-                    Board of Secondary Education,
-                  </h3>
-                  <p>Sacred Heart High School</p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                  iconStyle={{ background: "#1f163e", color: "#fff" }}
-                  icon={StartIcon}
-                />
-              </VerticalTimeline>
+                className="vertical-timeline-element--work"
+                contentStyle={{
+                  background: "#1f163e",
+                  color: "#fff",
+                }}
+                contentArrowStyle={{
+                  borderRight: "7px solid  rgb(33, 150, 243)",
+                }}
+                date="Aug 2024 - present"
+                iconStyle={{ background: "#1f163e", color: "#fff" }}
+                icon={WorkIcon}
+              >
+                <h3 className="vertical-timeline-element-title">Associate technical support engineer</h3>
+                <h4 className="vertical-timeline-element-subtitle">
+                  Salesforce India
+                </h4>
+                <p>
+                  Global leader in CRM software, driving customer relationships
+                  and business growth through cloud-based solutions and social
+                  responsibility initiatives.
+                </p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{
+                  color: "#000",
+                }}
+                contentArrowStyle={{
+                  borderRight: "7px solid  rgb(33, 150, 243)",
+                }}
+                date="Feb 2024 - Aug 2024"
+                iconStyle={{ background: "#1f163e", color: "#fff" }}
+                icon={WorkIcon}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  ATSE intern
+                </h3>
+                <h4 className="vertical-timeline-element-subtitle">
+                  Salesforce India
+                </h4>
+                <p>
+                  Global leader in CRM software, driving customer relationships
+                  and business growth through cloud-based solutions and social
+                  responsibility initiatives.
+                </p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{
+                  color: "#000",
+                }}
+                contentArrowStyle={{
+                  borderRight: "7px solid  rgb(33, 150, 243)",
+                }}
+                date="Aug 2023 - Feb 2024"
+                iconStyle={{ background: "#1f163e", color: "#fff" }}
+                icon={WorkIcon}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  National Software Engineering Fellow (GDC intern)
+                </h3>
+                <h4 className="vertical-timeline-element-subtitle">
+                  EGOVERNMENT FOUNDATION & PUPILFIRST PRIVATE LIMITED
+                </h4>
+                <p>
+                  Deputed to eGovernment foundation to develop Tele-ICU tooling
+                  for 10bedICU project.
+                </p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{
+                  color: "#000",
+                  marginRight: "10px",
+                }}
+                date="2020 - 2024"
+                iconStyle={{ background: "#1f163e", color: "#fff" }}
+                icon={SchoolIcon}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  Bachelor of Technology
+                </h3>
+                <h4 className="vertical-timeline-element-subtitle">
+                  Information Technology
+                </h4>
+                <p>Vardhaman college of Engineering, Hyderabad</p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{
+                  color: "#000",
+                }}
+                date="2018 - 2020"
+                iconStyle={{ background: "#1f163e", color: "#fff" }}
+                icon={SchoolIcon}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  Board of Intermediate
+                </h3>
+                <p>Narayana IIT academy</p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{
+                  color: "#000",
+                  marginRight: "10px",
+                }}
+                date="2018 "
+                iconStyle={{ background: "#1f163e", color: "#fff" }}
+                icon={SchoolIcon}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  Board of Secondary Education,
+                </h3>
+                <p>Sacred Heart High School</p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                iconStyle={{ background: "#1f163e", color: "#fff" }}
+                icon={StartIcon}
+              />
+            </VerticalTimeline>
             <br />
             <br />
 
             {/* <ScrollAnimation animateIn="fadeInLeft"> */}
-              <div className="tagline2">
-                I have become confident using the following technologies:
-              </div>
+            <div className="tagline2">
+              I have become confident using the following technologies:
+            </div>
             {/* </ScrollAnimation> */}
 
             <Technologies>
               {stackList.map((stack, index) => (
                 // <ScrollAnimation animateIn="fadeInLeft" key={index}>
-                  <Tech key={index} className="tech">
-                    <TechImg src={stack.img} alt={stack.name} />
-                    <TechName>{stack.name}</TechName>
-                  </Tech>
+                <Tech key={index} className="tech">
+                  <TechImg src={stack.img} alt={stack.name} />
+                  <TechName>{stack.name}</TechName>
+                </Tech>
                 // </ScrollAnimation>
               ))}
             </Technologies>
